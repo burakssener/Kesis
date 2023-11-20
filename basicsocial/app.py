784c5129554dc386dd7bfea996e82fe8152147ba
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-20 13:16:20 +0000

    Mon, Nov 20, 2023, 4:16 PM +03:00

--- basicsocial/app.py
+++ basicsocial/app.py
@@ -140,6 +140,8 @@ def buy():
 
 
 
-CREATE TABLE(
-    username text
-)
+CREATE TABLE users (
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+username TEXT NOT NULL,
+hash TEXT NOT NULL,
+age INTEGER NOT NULL);
