b7fa5253e0e44c21fc65a80bcb9b590761e1e2ff
Burak Sener <134919601+burakssener@users.noreply.github.com>
2023-11-25 08:06:59 +0000

    Sat, Nov 25, 2023, 11:06 AM +03:00

--- basicsocial/app.py
+++ basicsocial/app.py
@@ -216,7 +216,7 @@ def join(group_name):
     if request.method == "POST":
         password = request.form.get("password")
         group_info = db.execute("SELECT group_pass, group_id FROM groups WHERE group_name = ?", group_name)
-        if password == group_info[0]["group_pass"]:
+        if generate_password_hash(password) == group_info[0]["group_pass"]:
             group_id = int(group_info[0]["group_id"])
             db.execute("INSERT INTO group_members (user_id, group_id) VALUES (?, ?)", session["user_id"], group_id)
             return redirect(url_for('group_details', group_name = group_name))
